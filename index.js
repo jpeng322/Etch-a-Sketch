ae3fcd6d358ddff00d26e1169655f0b82c4ebc99
jacky <jackypengsmail@gmail.com>
2022-05-09 01:18:20 -0400

    Display hover mechanic on grid

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 const container = document.getElementById('container')
 
-function createDiv() {
-    for (i = 0; i < 16; i++) {
+function createDiv(rows) {
+    for (i = 0; i < rows; i++) {
         const boardDiv = document.createElement('div');
         boardDiv.className = "boardDiv";
         // console.log(boardDiv)
@@ -11,11 +11,11 @@ function createDiv() {
 
 }
 
-createDiv()
+createDiv(16)
 
-function createCol() {
-    for (a = 0; a < 16; a++) {
-        for (i = 0; i < 16; i++) {
+function createCol(cells) {
+    for (a = 0; a < cells; a++) {
+        for (i = 0; i < cells; i++) {
             const rowDiv = document.querySelectorAll('.boardDiv');
             const colDiv = document.createElement('div');
             colDiv.className = 'colDiv'
@@ -23,8 +23,28 @@ function createCol() {
         }
     }
 }
+createCol(16)
 
-createCol()
+
+function hover() {
+    this.style.backgroundColor = 'red'
+}
+
+document.getElementsByClassName
+let cells = document.getElementsByClassName('colDiv');
+console.log(cells[1])
+cells[0].addEventListener('mouseover', hover)
+
+function giveListener() {
+    for (i = 0; i < cells.length; i++) {
+        cells[i].addEventListener('mouseover', hover)
+    }
+}
+console.log(giveListener())
+
+
+
+
 
 // function createCells() {
 //     for (i = 0; i < 16; i++) {
